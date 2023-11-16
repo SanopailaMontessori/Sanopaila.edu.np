88a6529ec199ec24cb744ef2a1106da151ba0edb
SanopailaMontessori <montessori.sanopaila@gmail.com>
2023-11-16 19:22:24 +0545

    cleaned

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1700668500;s:7:"authKey";s:64:"MV=%~6cVTTap-s00t2<dDg@fu~!u&mwIYp7_-.lU2/5%mIYEP>cQ1EFW)A-?`UVz";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3846;}
